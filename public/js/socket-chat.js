ea5940fa3c2e82836f870ed8708421aef6c63026
Renato <rmontufar1792@gmail.com>
2020-05-30 00:02:18 -0600

    Primer commit

---
+++ public/js/socket-chat.js
@@ -0,0 +1,57 @@
+var socket = io();
+
+var params = new URLSearchParams(window.location.search);
+
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
+    throw new Error('El nombre o la sala es necesario');
+}
+
+var usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+}
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+    socket.emit('entrarChat', usuario, function(resp) {
+        //console.log('Usuarios conectados', resp);
+    });
+});
+
+// escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// Enviar información
+// socket.emit('crearMensaje', {
+//     usuario: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
+
+// Escuchar información
+socket.on('crearMensaje', function(mensaje) {
+
+    console.log('Servidor:', mensaje);
+
+});
+
+// Escuchar cuando un usuario entra o sale del chat
+socket.on('listaPersona', function(mensaje) {
+
+    console.log(mensaje);
+
+});
+
+// Mensaje privado
+socket.on('mensajePrivado', function(mensaje) {
+
+    console.log('mensajePrivado:', mensaje);
+
+});
